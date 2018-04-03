ec7e61856e7ab9f83e9f85b2677d81f748093811
everdude <bever01@gmail.com>
2018-04-02 21:59:08 -0400

    clean up metadata
    move site vars to config file

--- includes/metadata.php
+++ includes/metadata.php
@@ -3,17 +3,10 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1">
 <?php
-$uri_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$uri_segments = explode('/', $uri_path);
-//set homepage var
-if(empty($uri_segments[1])){
-    $home = "1";
-}
-$pageTitle = "CSS Grid Layout Example";
-
 if ($gallery == true) {
 ?>
 <link rel="stylesheet" href="/css/colorbox.css">
 <?php
 }
 ?>
+<link href="https://fonts.googleapis.com/css?family=Merriweather|Vollkorn" rel="stylesheet">
