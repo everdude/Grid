56230933182956d7269b6291796d64dec636878d
everdude <bever01@gmail.com>
2018-04-02 22:00:49 -0400

    web development
    setup web dev page with examples and skills

---
+++ web-development.php
@@ -0,0 +1,60 @@
+<?php require_once("config.php") ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php require_once(SITE_ROOT.'/includes/metadata.php'); ?>
+        <title>Web Development - <?php echo $pageTitle; ?></title>
+        <meta name="keywords" content="">
+        <meta name ="descrption" content="">
+    </head>
+    <?php require_once(SITE_ROOT.'/includes/header.php'); ?>
+        <article id="content">
+            <h1>Web Development</h1>
+            <h2>Sites</h2>
+            <ul>
+                <li>
+                    <h3><a href="">canr site. migration, Development, response design</a></h3>
+                    <p>responsible for implementing dozens of responsive sites before switching to common template.</p>
+                </li>
+                <li>
+                    <h3><a href="">Michigan Agriculture Environmental Assurance Program</a></h3>
+                    <p>Add summary</p>
+                </li>
+                <li>
+                    <h3><a href="">mshn design Development</a></h3>
+                    <p>Add summary</p>
+                </li>
+                <li>
+                    <h3><a href="">spartan printing Development</a></h3>
+                    <p>developent. set up wordpress blog</p>
+                </li>
+                <li>
+                    <h3><a href="">Gratiot County Community Mental Health - now gratiot integrated health network</a></h3>
+                    <p>Add summary</p>
+                </li>
+                <li>
+                    <h3><a href="">jackson community college</a></h3>
+                    <p>Add summary</p>
+                </li>
+            </ul>
+
+        </article>
+        <aside id="sideBar">
+            <h2>Skills &amp; Tools</h2>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>CSS Grid Layout</li>
+                <li>Response Design</li>
+                <li>Git</li>
+                <li>JavaScript</li>
+                <li>PHP</li>
+                <li>Velocity</li>
+                <li>SASS</li>
+                <li>Adobe Creative Suite</li>
+                <li>jQuery</li>
+            </ul>
+        </aside>
+        <?php require_once(SITE_ROOT.'/includes/footer.php'); ?>
+    </body>
+</html>
