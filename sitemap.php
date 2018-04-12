8726a9c7fa3cebfd741e159a4f8e849a77bb6425
everdude <bever01@gmail.com>
2018-04-11 22:00:35 -0400

    sitemap
    add class to sitemap so style it

--- sitemap.php
+++ sitemap.php
@@ -8,7 +8,7 @@
         <meta name ="descrption" content="">
     </head>
     <?php require_once(SITE_ROOT.'/includes/header.php'); ?>
-        <article id="content">
+        <article id="content" class="sitemap">
             <h1>Sitemap</h1>
             <?php include(SITE_ROOT.'/includes/pages.php'); ?>
         </article>
