d5be4ee084a589ce46da6c66583d46d146697e66
everdude <bever01@gmail.com>
2018-04-11 21:58:17 -0400

    contact ids
    update inputs w/ ids so labels function

--- contact.php
+++ contact.php
@@ -14,18 +14,18 @@ $captcha = true;
     <?php require_once(SITE_ROOT.'/includes/header.php'); ?>
         <article id="content">
             <h1>Contact</h1>
-            <form class="" action="processEmail.php" method="post">
+            <form class="contactForm" action="processEmail.php" method="post">
                 <p>
-                    <label for="name">* Name:</label>
-                    <input type="text" required name="name" value="">
+                    <label for="name">Name:</label>
+                    <input type="text" id="name" required name="name" value="">
                 </p>
                 <p>
-                    <label for="email">* Email:</label>
-                    <input type="email" required name="email" value="">
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" required name="email" value="">
                 </p>
                 <p class="">
-                    <label for="message">* Message:</label>
-                    <textarea name="message" required rows="8"></textarea>
+                    <label for="message">Message:</label>
+                    <textarea name="message" id="message" required rows="8"></textarea>
                 </p>
                 <div class="g-recaptcha" data-sitekey="6LfItkoUAAAAAIw2v30MBnr7Z-SdXtQZJsz-EXjx"></div>
                 <p><input type="submit" value="Submit"></p>
