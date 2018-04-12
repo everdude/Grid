a4922927b21e05f8f64ea46638f31723b476f27c
everdude <bever01@gmail.com>
2018-04-11 22:00:01 -0400

    updates nav
    add/reorder graphic design and fine art gallery pages

--- includes/pages.php
+++ includes/pages.php
@@ -1,7 +1,8 @@
 <ul>
     <li class="level_1<?php if($uri_segments[1] == ""){?> active<?php }?>"><a href="/">Home</a></li>
     <li class="level_1<?php if($uri_segments[1] == "web-development"){?> active<?php }?>"><a href="/web-development">Web Development</a></li>
+    <li class="level_1<?php if($uri_segments[1] == "graphic-design"){?> active<?php }?>"><a href="graphic-design">Graphic Design</a></li>
+    <li class="level_1<?php if($uri_segments[1] == "fine-art-gallery"){?> active<?php }?>"><a href="/fine-art-gallery">Fine Art Gallery</a></li>
     <li class="level_1<?php if($uri_segments[1] == "articles"){?> active<?php }?>"><a href="/articles">Articles</a></li>
-    <li class="level_1<?php if($uri_segments[1] == "image-gallery"){?> active<?php }?>"><a href="/image-gallery">Image Gallery</a></li>
     <li class="level_1<?php if($uri_segments[1] == "contact"){?> active<?php }?>"><a href="/contact">Contact</a></li>
 </ul>
