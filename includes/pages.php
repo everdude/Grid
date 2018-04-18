3a742cb796855dca3ad2ac20032e9f4bd537780d
everdude <bever01@gmail.com>
2018-04-17 21:37:17 -0400

    resume

--- includes/pages.php
+++ includes/pages.php
@@ -4,5 +4,6 @@
     <li class="level_1<?php if($uri_segments[1] == "graphic-design"){?> active<?php }?>"><a href="graphic-design">Graphic Design</a></li>
     <li class="level_1<?php if($uri_segments[1] == "fine-art-gallery"){?> active<?php }?>"><a href="/fine-art-gallery">Fine Art Gallery</a></li>
     <li class="level_1<?php if($uri_segments[1] == "articles"){?> active<?php }?>"><a href="/articles">Articles</a></li>
+    <li class="level_1<?php if($uri_segments[1] == "resume"){?> active<?php }?>"><a href="/resume">Resume</a></li>
     <li class="level_1<?php if($uri_segments[1] == "contact"){?> active<?php }?>"><a href="/contact">Contact</a></li>
 </ul>
---
+++ resume.php
@@ -0,0 +1,116 @@
+<?php require_once("config.php");?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php require_once(SITE_ROOT.'/includes/metadata.php'); ?>
+        <title>Resume - <?php echo $siteTitle; ?></title>
+        <meta name="keywords" content="">
+        <meta name ="descrption" content="">
+    </head>
+    <?php require_once(SITE_ROOT.'/includes/header.php'); ?>
+        <article id="content">
+            <h1>Resume</h1>
+            <section>
+                <h2>Recent Accomplishmenets</h2>
+                    <ul>
+                        <li>Created dozens of custom responsive templates </li>
+                        <li>Migrated dozens of websites into a common responsive template,  saving design and development time and resources</li>
+                        <li>Created custom content types, information architecture and taxonomies unifying hundreds of website structures under one umbrella, allowing for rapid development and feature rollout.</li>
+                        <li>Implement Schema into content types to increase SEO for hundreds of websites.</li>
+                        <li> Lead the migration of dozens of websites into a new content management system. Met with stakeholders of the websites being migrated and set up schedules and coordinated migration efforts.</li>
+                        <li>Created custom reusable dynamic content listings and pieces of functionality that can utilize custom APIs from different systems to be reused across hundreds of websites. </li>
+                    </ul>
+            </section>
+            <section>
+                <h2>Skills</h2>
+                <ul>
+			<li>Prepress and design aptitude.</li>
+			<li>Experiential implementation of computer graphic software training.</li>
+			<li>Communication with clients to produce problem solving solutions to exceed the clients needs.</li>
+			<li>Able to work in a self starting environment.</li>
+			<li>Cross platform knowledge and experience of both Mac and PC computers.</li>
+			<li>Creating prepress and other media specific ready projects.</li>
+			<li>Working towards group goals in a team oriented atmosphere.</li>
+			<li>Create effective traffic and sales producing websites.</li>
+			<li>Adobe CS3, QuarkXpress, Pitstop, Quite Imposing, Publisher, Dreamweaver, and Flash.</li>
+			<li>HTML, CSS, and PHP.</li>
+		</ul>
+            </section>
+            <section>
+                <h2>Work Experience</h2>
+                <h3>Front End Web Developer</h3>
+                <p>Michigan State University</p>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+                <h3>Web Designer/Developer</h3>
+                <p>Jackson Community College, Jackson, MI, 04/08 &ndash; 06/12</p>
+		<ul>
+			<li>Maintain the website with regular updates, error checking, and code review.</li>
+			<li>Communicate with other departments to help develop different sections of the website.</li>
+			<li>Designed a new home page, making the navigation more consistent and simple.</li>
+					</ul>
+        <h3>Graphic Design/Prepress Technician</h3>
+        <p>Allegra Print and Imaging, Lansing, MI 07/07 &ndash; 04/08</p>
+		<ul>
+			<li>Implemented new file storage and filing system.</li>
+			<li>Pre-flight, impose, and output jobs for printing.</li>
+			<li>Oversee jobs in the pre-press department and kept accurate scheduling of jobs.</li>
+			<li>Design marketing pieces and mailings for clients, along with standard with business identification stationary.</li>
+		</ul>
+        <h3>Graphic Design/Prepress Manager</h3>
+		<p>A&amp;E Printers and Mailers, Inc., Lansing, MI 01/05 &ndash; 06/07</p>
+		<ul>
+			<li>Supervise and manage jobs in the workflow, seeing to timely and accurate production.</li>
+			<li>Design and setup a diversity of newsletters for distinct clients.</li>
+			<li>Pre-flight and repair client’s designs for press.</li>
+			<li>Planned, executed, and constructed multiple websites for companies.</li>
+			<li>Work with clients to design, and achieve effective marketing promotional pieces.</li>
+		</ul>
+        <h3>Online Tutorials Designer</h3>
+		<p>Metasurge, Lansing, MI 08/04 &ndash; 01/05</p>
+		<ul>
+			<li>Create and produce online Flash based H.R. training tutorials, used in various companies to bring their employees up to date in knowledge and safety
+			through interactive multimedia.</li>
+		</ul>
+        <h3>3D Modeler Intern</h3>
+		<p>Blue L Media LLC, Diamondale, MI 05/04 &ndash; 08/04</p>
+		<ul>
+			<li>Constructed complex digital 3d models, in LightWave, for use in training simulations.</li>
+			<li>Created interactive Flash marking piece.</li>
+		</ul>
+        <h3>Freelance Designer</h3>
+		<p>Lansing, MI 02/02 &ndash; Present</p>
+		<ul>
+			<li>Produced and setup bus map brochures for M.D.O.T. for the city of Detroit in Arabic.</li>
+			<li>Designed brochures, slides, and logos for Mosaic, a collegiate ministry.</li>
+			<li>Designed a CD cover for local rock artist.</li>
+		</ul>
+        <h3>Graphic Artist</h3>
+		<p>Orange County Graphic Reproductions, Orlando, FL. 06/02 &ndash; 08/03, Summers</p>
+		<ul class="list">
+			<li>Created and designed an innovative digital marketing promotional CD Rom business card to promote the press shop.</li>
+			<li>Created an informational, community connecting display for public access.</li>
+			<li>Constructed a promotional graphic cover for County's Budget Briefing Session for Fiscal Year 2003.</li>
+			<li>Designed brochures and inserts for various government programs.</li>
+			<li>Re-set numerous forms used daily by county personnel.</li>
+            </section>
+            <section>
+                <h2>Education</h2>
+                <p>Siena Heights University, MI<br/>
+        		Bachelor's Degree, Major Communications</p>
+        		<p>Lansing Community College, Lansing, MI<br/>
+        		Associate Degree, Applied Arts. Major, Computer Graphics, Multimedia.</p>
+        		<p>General Associate Degree</p>
+            </section>
+        </article>
+        <aside id="sideBar">
+
+        </aside>
+        <?php require_once(SITE_ROOT.'/includes/footer.php'); ?>
+    </body>
+</html>
